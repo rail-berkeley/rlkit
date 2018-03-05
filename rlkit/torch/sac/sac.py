b1eaa59f8d521688c437b61c67edb91af8421548
Vitchyr Pong <vitchyr@gmail.com>
2018-03-05 15:37:45 -0800

    remove old comment

--- rlkit/torch/sac/sac.py
+++ rlkit/torch/sac/sac.py
@@ -1,23 +1,3 @@
-"""
-Soft actor-critic
-
-Notes from Tuomas
-a = tanh(z)
-Q(a, s) = Q(tanh(z), s)
-z is output of actor
-
-Policy
-pi ~ e^{Q(s, a)}
-   ~ e^{Q(s, tanh(z))}
-
-Mean
- - regularize gaussian means of policy towards zeros
-
-Covariance
- - output log of diagonals
- - Clip the log of the diagonals
- - regularize the log towards zero
-"""
 from collections import OrderedDict
 
 import numpy as np
