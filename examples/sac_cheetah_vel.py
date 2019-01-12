f0b8d6080ef47a49b7acfdd2946766162d681459
Kate Rakelly <krakelly@berkeley.edu>
2019-01-12 14:15:53 -0800

    scripts: update cheetah scripts to the present day

--- examples/sac_cheetah_vel.py
+++ examples/sac_cheetah_vel.py
@@ -2,6 +2,7 @@
 Run Prototypical Soft Actor Critic on HalfCheetahEnv.
 
 """
+import os
 import numpy as np
 import click
 import datetime
@@ -9,12 +10,13 @@ import pathlib
 from gym.envs.mujoco import HalfCheetahEnv
 
 from rlkit.envs.half_cheetah_vel import HalfCheetahVelEnv
-import rlkit.torch.pytorch_util as ptu
 from rlkit.envs.wrappers import NormalizedBoxEnv
 from rlkit.launchers.launcher_util import setup_logger
-from rlkit.torch.sac.policies import ProtoTanhGaussianPolicy
-from rlkit.torch.networks import FlattenMlp
+from rlkit.torch.sac.policies import TanhGaussianPolicy
+from rlkit.torch.networks import FlattenMlp, MlpEncoder, RecurrentEncoder
 from rlkit.torch.sac.sac import ProtoSoftActorCritic
+from rlkit.torch.sac.proto import ProtoAgent
+import rlkit.torch.pytorch_util as ptu
 
 def datetimestamp(divider=''):
     now = datetime.datetime.now()
@@ -34,11 +36,12 @@ def experiment(variant):
     reward_dim = 1
 
     net_size = variant['net_size']
-    # start with linear task encoding
-    task_enc = FlattenMlp(
-        hidden_sizes=[200, 200, 200], # deeper net + higher dim space generalize better
-        input_size=obs_dim + reward_dim,
-        output_size=task_enc_output_dim,
+    recurrent = variant['algo_params']['recurrent']
+    encoder_model = RecurrentEncoder if recurrent else MlpEncoder
+    task_enc = encoder_model(
+            hidden_sizes=[200, 200, 200], # deeper net + higher dim space generalize better
+            input_size=obs_dim + reward_dim,
+            output_size=task_enc_output_dim,
     )
     qf1 = FlattenMlp(
         hidden_sizes=[net_size, net_size, net_size],
@@ -55,7 +58,7 @@ def experiment(variant):
         input_size=obs_dim + latent_dim,
         output_size=1,
     )
-    policy = ProtoTanhGaussianPolicy(
+    policy = TanhGaussianPolicy(
         hidden_sizes=[net_size, net_size, net_size],
         obs_dim=obs_dim + latent_dim,
         latent_dim=latent_dim,
@@ -68,15 +71,21 @@ def experiment(variant):
         output_size=1
     )
 
+    agent = ProtoAgent(
+        latent_dim,
+        [task_enc, policy, qf1, qf2, vf, rf],
+        **variant['algo_params']
+    )
+
     algorithm = ProtoSoftActorCritic(
         env=env,
         train_tasks=tasks[:-10],
         eval_tasks=tasks[-10:],
-        nets=[task_enc, policy, qf1, qf2, vf, rf],
+        nets=[agent, task_enc, policy, qf1, qf2, vf, rf],
         latent_dim=latent_dim,
         **variant['algo_params']
     )
-    algorithm.cuda()
+    algorithm.to()
     algorithm.train()
 
 
@@ -84,8 +93,7 @@ def experiment(variant):
 @click.argument('gpu', default=0)
 @click.option('--docker', default=0)
 def main(gpu, docker):
-    log_dir = '/mounts/output' if docker == 1 else 'output'
-    max_path_length = 100
+    max_path_length = 1000
     # noinspection PyTypeChecker
     variant = dict(
         task_params=dict(
@@ -98,8 +106,11 @@ def main(gpu, docker):
             num_tasks_sample=5,
             num_steps_per_task=5 * max_path_length,
             num_train_steps_per_itr=1000,
+            num_evals=3, # number of evals with separate task encodings
             num_steps_per_eval=10 * max_path_length,
             batch_size=256, # to compute training grads from
+            embedding_batch_size=1000,
+            embedding_mini_batch_size=100,
             max_path_length=max_path_length,
             discount=0.99,
             soft_target_tau=0.005,
@@ -112,20 +123,25 @@ def main(gpu, docker):
             use_information_bottleneck=False,
             train_embedding_source='online_exploration_trajectories',
             eval_embedding_source='online_exploration_trajectories',
+            recurrent=True, # recurrent or averaging encoder
         ),
         net_size=300,
         use_gpu=True,
         gpu_id=gpu,
     )
-    exp_name = 'proto-sac-half-cheetah-vel-16z-batch16'
+    exp_name = 'proto-sac-recurrent-ep1000'
 
     log_dir = '/mounts/output' if docker == 1 else 'output'
-    experiment_log_dir = setup_logger(exp_name, variant=variant, base_log_dir=log_dir)
+    experiment_log_dir = setup_logger(exp_name, variant=variant, exp_id='half-cheetah-vel', base_log_dir=log_dir)
 
     # creates directories for pickle outputs of trajectories (point mass)
     pickle_dir = experiment_log_dir + '/eval_trajectories'
     pathlib.Path(pickle_dir).mkdir(parents=True, exist_ok=True)
     variant['algo_params']['output_dir'] = pickle_dir
+
+    # debugging triggers a lot of printing
+    DEBUG = 0
+    os.environ['DEBUG'] = str(DEBUG)
 
     experiment(variant)
 
