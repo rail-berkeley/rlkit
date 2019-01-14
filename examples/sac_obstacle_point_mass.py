b2e183433c3fd07b8c41cf5365385c8941022bd5
Aurick Zhou <azhou42@berkeley.edu>
2019-01-14 00:03:19 -0800

    add point env obstacles env with script, script may be out of date

---
+++ examples/sac_obstacle_point_mass.py
@@ -0,0 +1,139 @@
+"""
+Run Prototypical Soft Actor Critic on point mass obstacle.
+
+"""
+import numpy as np
+import click
+import datetime
+import pathlib
+
+from rlkit.envs.point_mass_obstacles import PointEnvObstacles
+from rlkit.envs.wrappers import NormalizedBoxEnv
+from rlkit.launchers.launcher_util import setup_logger
+from rlkit.torch.sac.policies import ProtoTanhGaussianPolicy
+from rlkit.torch.networks import FlattenMlp
+from rlkit.torch.sac.sac import ProtoSoftActorCritic
+import rlkit.torch.pytorch_util as ptu
+
+def datetimestamp(divider=''):
+    now = datetime.datetime.now()
+    return now.strftime('%Y-%m-%d-%H-%M-%S-%f').replace('-', divider)
+
+def experiment(variant):
+    env = NormalizedBoxEnv(PointEnv(**variant['task_params']))
+    ptu.set_gpu_mode(variant['use_gpu'], variant['gpu_id'])
+
+    tasks = env.get_all_task_idx()
+
+    obs_dim = int(np.prod(env.observation_space.shape))
+    action_dim = int(np.prod(env.action_space.shape))
+    latent_dim = 5
+    task_enc_output_dim = latent_dim * 2 if variant['algo_params']['use_information_bottleneck'] else latent_dim
+    reward_dim = 1
+
+    net_size = variant['net_size']
+    # start with linear task encoding
+    task_enc = FlattenMlp(
+            hidden_sizes=[200, 200, 200], # deeper net + higher dim space generalize better
+            input_size=obs_dim + reward_dim,
+            output_size=task_enc_output_dim,
+    )
+    qf1 = FlattenMlp(
+        hidden_sizes=[net_size, net_size, net_size],
+        input_size=obs_dim + action_dim + latent_dim,
+        output_size=1,
+    )
+    qf2 = FlattenMlp(
+        hidden_sizes=[net_size, net_size, net_size],
+        input_size=obs_dim + action_dim + latent_dim,
+        output_size=1,
+    )
+    vf = FlattenMlp(
+        hidden_sizes=[net_size, net_size, net_size],
+        input_size=obs_dim + latent_dim,
+        output_size=1,
+    )
+    policy = ProtoTanhGaussianPolicy(
+        hidden_sizes=[net_size, net_size, net_size],
+        obs_dim=obs_dim + latent_dim,
+        latent_dim=latent_dim,
+        action_dim=action_dim,
+    )
+
+    rf = FlattenMlp(
+        hidden_sizes=[net_size, net_size, net_size],
+        input_size=obs_dim + latent_dim,
+        output_size=1
+    )
+
+    algorithm = ProtoSoftActorCritic(
+        env=env,
+        train_tasks=list(tasks[:-10]),
+        eval_tasks=list(tasks[-10:]),
+        nets=[task_enc, policy, qf1, qf2, vf, rf],
+        latent_dim=latent_dim,
+        **variant['algo_params']
+    )
+    if ptu.gpu_enabled():
+        algorithm.cuda()
+    algorithm.train()
+
+
+@click.command()
+@click.argument('gpu', default=0)
+@click.option('--docker', default=0)
+def main(gpu, docker):
+    max_path_length = 20
+    # noinspection PyTypeChecker
+    for i, kl_lambda in enumerate([0.01, 0.1, 1., 10., 100., 1000., 10000.]):
+        variant = dict(
+            task_params=dict(
+                n_tasks=100,
+                randomize_tasks=True,
+            ),
+            algo_params=dict(
+                meta_batch=10,
+                num_iterations=1000,
+                num_tasks_sample=5,
+                num_steps_per_task=10 * max_path_length,
+                num_train_steps_per_itr=1000,
+                num_steps_per_eval=10 * max_path_length,  # num transitions to eval on
+                embedding_batch_size=256,
+                batch_size=256,  # to compute training grads from
+                max_path_length=max_path_length,
+                discount=0.99,
+                soft_target_tau=0.005,
+                policy_lr=3E-4,
+                qf_lr=3E-4,
+                vf_lr=3E-4,
+                context_lr=3e-4,
+                reward_scale=100.,
+                reparameterize=True,
+                kl_lambda=.1,
+                rf_loss_scale=1.,
+                use_information_bottleneck=True,  # only supports False for now
+
+                train_embedding_source='online_exploration_trajectories',
+                # embedding_source should be chosen from
+                # {'initial_pool', 'online_exploration_trajectories', 'online_on_policy_trajectories'}
+                eval_embedding_source='online_exploration_trajectories',
+            ),
+            net_size=300,
+            use_gpu=True,
+            gpu_id=gpu,
+        )
+
+        exp_name = 'proto-sac/point-mass/kl_lambda_sweep/{}'.format(i)
+
+        log_dir = '/mounts/output' if docker == 1 else 'output'
+        experiment_log_dir = setup_logger(exp_name, variant=variant, base_log_dir=log_dir)
+
+        # creates directories for pickle outputs of trajectories (point mass)
+        pickle_dir = experiment_log_dir + '/eval_trajectories'
+        pathlib.Path(pickle_dir).mkdir(parents=True, exist_ok=True)
+        variant['algo_params']['output_dir'] = pickle_dir
+
+        experiment(variant)
+
+if __name__ == "__main__":
+    main()
---
+++ rlkit/envs/point_env_obstacles.py
@@ -0,0 +1,110 @@
+import numpy as np
+from gym import spaces
+from gym import Env
+
+
+class PointEnvObstacles(Env):
+    """
+    point mass on a 2-D plane
+    two tasks: move to (-1, -1) or move to (1,1)
+    """
+
+    def __init__(self, task={'direction': 1}, randomize_tasks=False, n_tasks=2, n_obstacles=0, obstacle_radius=0.2):
+        # directions = [-1, 0, 1, 2, 3, 4, 5, 6]
+        directions = list(range(n_tasks))
+
+        if randomize_tasks:
+            goals = [[np.random.uniform(-1., 1.), np.random.uniform(-1., 1.)] for _ in directions]
+            obstacles = = [[np.random.uniform(-1., 1., 2) for o in obstacle] for _ in directions]
+
+            # goals = [1 * np.random.uniform(-1., 1., 2) for _ in directions]
+        else:
+            # add more goals in n_tasks > 7
+            goals = [np.array([10, -10]),
+                     np.array([10, 10]),
+                     np.array([-10, 10]),
+                     np.array([-10, -10]),
+                     np.array([0, 0]),
+
+                     np.array([7, 2]),
+                     np.array([0, 4]),
+                     np.array([-6, 9])
+                     ]
+        self.goals = goals
+        self.obstacles = obstacles
+
+        self.tasks = [{'direction': direction} for direction in directions]
+        self.n_obstacles= n_obstacles
+        self.obstacle_radius = obstacle_radius
+        self._task = task
+        self._goal = self.reset_goal(task.get('direction', 1))
+        self._obstacle = self.reset_obstacle(task.get('direction', 1))
+        self.reset_model()
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(2,))
+        self.action_space = spaces.Box(low=-0.1, high=0.1, shape=(2,))
+
+    def reset_task(self, idx):
+        self._task = self.tasks[idx]
+        self._goal = self.reset_goal(self._task['direction'])
+        self._obstacle = self.reset_obstacle(self._task['direction'])
+        self.reset()
+
+    def reset_goal(self, direction):
+        return self.goals[direction]
+        """
+        if direction == 6:
+            return np.array([-6., 9.]) # 1,1 and -1,-1 originally
+        if direction == 5:
+            return np.array([0., 4.]) # 1,1 and -1,-1 originally
+        if direction == 4:
+            return np.array([7., 2.]) # 1,1 and -1,-1 originally
+        if direction == 2:
+            return np.array([-10., -10.]) # 1,1 and -1,-1 originally
+        elif direction == 1:
+            return np.array([5, 10]) # 1,1 and -1,-1 originally
+        elif direction == 0:
+            return np.array([10, -10])
+        elif direction == 3:
+            return np.array([0., 0.])
+        else:
+            return np.array([-10, 0])
+        """
+
+    def get_all_task_idx(self):
+        return range(len(self.tasks))
+
+    def reset_model(self):
+        self._state = np.random.uniform(-1., 1., size=(2,))
+        return self._get_obs()
+
+    def reset(self):
+        return self.reset_model()
+
+    def _get_obs(self):
+        return np.copy(self._state)
+
+    def _handle_obstacles(self, new_s):
+        for center in self.obstacle_centers:
+            # obstacles are a self.radius ball around (a,b)
+            dist = np.linalg.norm(new_s - center) 
+            assert dist > 0
+            if dist > self.obstacle_radius:
+                return center + (new_s - center) / dist * self.obstacle_radius
+        return new_s
+
+    def step(self, action):
+        self._state = self._handle_obstacles(self._state + action)
+        x, y = self._state
+        x -= self._goal[0]
+        y -= self._goal[1]
+        reward = - (x ** 2 + y ** 2) ** 0.5
+        done = False # abs(x) < 0.01 and abs(y) < 0.01
+        ob = self._get_obs()
+        return ob, reward, done, dict()
+
+    def viewer_setup(self):
+        print('no viewer')
+        pass
+
+    def render(self):
+        print('current state:', self._state)
