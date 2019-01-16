FIX!! apply gradients from KL loss
also log the mean and variance of z

rlkit/torch/sac/proto.py
@@ -9,11 +9,10 @@ import rlkit.torch.pytorch_util as ptu
 from rlkit.torch.core import np_ify, torch_ify
 
 
-def _product_of_gaussians(mus, sigmas):
+def _product_of_gaussians(mus, sigmas_squared):
     '''
     compute mu, sigma of product of gaussians
     '''
-    sigmas_squared = sigmas ** 2
     sigma_squared = 1. / torch.sum(torch.reciprocal(sigmas_squared), dim=0)
     mu = sigma_squared * torch.sum(mus / sigmas_squared, dim=0)
     return mu, torch.sqrt(sigma_squared)
@@ -86,8 +85,8 @@ class ProtoAgent(nn.Module):
     def information_bottleneck(self, z):
         # assume input and output to be task x batch x feat
         mu = z[..., :self.latent_dim]
-        sigma = F.softplus(z[..., self.latent_dim:])
-        z_params = [_product_of_gaussians(m, s) for m, s in zip(torch.unbind(mu), torch.unbind(sigma))]
+        sigma_squared = F.softplus(z[..., self.latent_dim:])
+        z_params = [_product_of_gaussians(m, s) for m, s in zip(torch.unbind(mu), torch.unbind(sigma_squared))]
         if not self.det_z:
             z_dists = [torch.distributions.Normal(m, s) for m, s in z_params]
             self.z_dists = z_dists

rlkit/torch/sac/sac.py
@@ -170,6 +170,7 @@ class ProtoSoftActorCritic(MetaTorchRLAlgorithm):
         new_actions, policy_mean, policy_log_std, log_pi = policy_outputs[:4]
 
         # KL constraint on z if probabilistic
+        self.context_optimizer.zero_grad()
         if self.use_information_bottleneck:
             kl_div = self.policy.compute_kl_div()
             kl_loss = self.kl_lambda * kl_div
@@ -179,7 +180,6 @@ class ProtoSoftActorCritic(MetaTorchRLAlgorithm):
         rewards_enc_flat = rewards_enc.view(self.embedding_mini_batch_size * num_tasks, -1)
         rf_loss = self.rf_loss_scale * self.rf_criterion(r_pred, rewards_enc_flat)
         self.rf_optimizer.zero_grad()
-        self.context_optimizer.zero_grad()
         rf_loss.backward(retain_graph=True)
         self.rf_optimizer.step()
 
@@ -236,7 +236,14 @@ class ProtoSoftActorCritic(MetaTorchRLAlgorithm):
         if self.eval_statistics is None:
             # eval should set this to None.
             # this way, these statistics are only computed for one batch.
+            # TODO this is kind of annoying and higher variance, why not just average
+            # across all the train steps?
             self.eval_statistics = OrderedDict()
+            z_mean = np.mean(np.abs(ptu.get_numpy(self.policy.z_dists[0].mean)))
+            z_sig = np.mean(ptu.get_numpy(self.policy.z_dists[0].variance))
+            self.eval_statistics['Z mean train'] = z_mean
+            self.eval_statistics['Z variance train'] = z_sig
+
             self.eval_statistics['QF Loss'] = np.mean(ptu.get_numpy(qf_loss))
             self.eval_statistics['VF Loss'] = np.mean(ptu.get_numpy(vf_loss))
             self.eval_statistics['RF Loss'] = np.mean(ptu.get_numpy(rf_loss))
@@ -245,6 +252,7 @@ class ProtoSoftActorCritic(MetaTorchRLAlgorithm):
             ))
             if self.use_information_bottleneck:
                 self.eval_statistics['KL Divergence'] = ptu.get_numpy(kl_div)
+                self.eval_statistics['KL Loss'] = ptu.get_numpy(kl_loss)
             self.eval_statistics.update(create_stats_ordered_dict(
                 'Q Predictions',
                 ptu.get_numpy(q1_pred),

rlkit/torch/torch_rl_algorithm.py
@@ -159,8 +159,7 @@ class MetaTorchRLAlgorithm(MetaRLAlgorithm, metaclass=abc.ABCMeta):
         for idx in self.train_tasks:
             dprint('task {} encoder RB size'.format(idx), self.enc_replay_buffer.task_buffers[idx]._size)
             paths = self.collect_paths(idx, eval_task=False)
-            self.log_statistics(paths, split='train')
-            train_avg_returns.append(self.eval_statistics['AverageReturn_{}_task{}'.format('train', idx)])
+            train_avg_returns.append(eval_util.get_average_returns(paths))
 
         ### test tasks
         dprint('evaluating on {} test tasks'.format(len(self.eval_tasks)))
@@ -193,8 +192,13 @@ class MetaTorchRLAlgorithm(MetaRLAlgorithm, metaclass=abc.ABCMeta):
             test_paths = []
             for _ in range(self.num_evals):
                 test_paths += self.collect_paths(idx, eval_task=True)
-            self.log_statistics(test_paths, split='test')
-            test_avg_returns.append(self.eval_statistics['AverageReturn_{}_task{}'.format('test', idx)])
+
+            z_mean = np.mean(np.abs(ptu.get_numpy(self.policy.z_dists[0].mean)))
+            z_sig = np.mean(ptu.get_numpy(self.policy.z_dists[0].variance))
+            self.eval_statistics['Z mean eval'] = z_mean
+            self.eval_statistics['Z variance eval'] = z_sig
+
+            test_avg_returns.append(eval_util.get_average_returns(test_paths))
 
             # TODO(KR) what does this do
             if hasattr(self.env, "log_diagnostics"):
@@ -208,6 +212,7 @@ class MetaTorchRLAlgorithm(MetaRLAlgorithm, metaclass=abc.ABCMeta):
 
         for key, value in self.eval_statistics.items():
             logger.record_tabular(key, value)
+        self.eval_statistics = None
 
         if self.render_eval_paths:
             self.env.render_paths(test_paths)