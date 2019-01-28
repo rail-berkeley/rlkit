9803d0981ea6fb3c90c7c918953ed9b6416e0f0b
Vitchyr Pong <vitchyr@gmail.com>
2019-01-27 17:24:10 -0800

    fix HER examples so that they run

--- examples/her/her_td3_multiworld_sawyer_reach.py
+++ examples/her/her_td3_multiworld_sawyer_reach.py
@@ -7,6 +7,7 @@ a bit noisy from one epoch to the next (occasionally dips dow to ~2000).
 Note that one epoch = 5k steps, so 200 epochs = 1 million steps.
 """
 import gym
+import multiworld.envs.mujoco  # Trigger env registration
 
 import rlkit.torch.pytorch_util as ptu
 from rlkit.data_management.obs_dict_replay_buffer import ObsDictRelabelingBuffer
@@ -57,12 +58,18 @@ def experiment(variant):
         **variant['replay_buffer_kwargs']
     )
     algorithm = HerTd3(
-        env=env,
-        qf1=qf1,
-        qf2=qf2,
-        policy=policy,
-        exploration_policy=exploration_policy,
         replay_buffer=replay_buffer,
+        her_kwargs=dict(
+            observation_key='observation',
+            desired_goal_key='desired_goal'
+        ),
+        td3_kwargs = dict(
+            env=env,
+            qf1=qf1,
+            qf2=qf2,
+            policy=policy,
+            exploration_policy=exploration_policy
+        ),
         **variant['algo_kwargs']
     )
     algorithm.to(ptu.device)
