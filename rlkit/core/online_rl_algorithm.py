915d7c840d1f547d978fab57bed088702e1c6459
Hexiang Hu <frank.hexiang@gmail.com>
2019-04-09 22:29:22 -0700

    Add online rl algorithm + train_mode function
    Make compatible with most recent data_collector
    
    Fix Multiple Functions
    
    (1) Add train_mode to batch_rl_algorithm
    (2) Make online_rl_algorithm compatible with new data collector api

---
+++ rlkit/core/online_rl_algorithm.py
@@ -0,0 +1,199 @@
+from collections import OrderedDict
+
+import gtimer as gt
+
+from rlkit.core import logger, eval_util
+from rlkit.data_management.replay_buffer import ReplayBuffer
+from rlkit.samplers.data_collector import PathCollector
+
+
+def _get_epoch_timings(epoch):
+    times_itrs = gt.get_times().stamps.itrs
+    times = OrderedDict()
+    epoch_time = 0
+    for key in sorted(times_itrs):
+        time = times_itrs[key][-1]
+        epoch_time += time
+        times['time/{} (s)'.format(key)] = time
+    times['time/epoch (s)'] = epoch_time
+    times['time/total (s)'] = gt.get_times().total
+    return times
+
+
+class OnlineRLAlgorithm(object):
+    def __init__(
+            self,
+            trainer,
+            exploration_env,
+            evaluation_env,
+            exploration_data_collector: PathCollector,
+            evaluation_data_collector: PathCollector,
+            replay_buffer: ReplayBuffer,
+            batch_size,
+            max_path_length,
+            num_epochs,
+            num_eval_steps_per_epoch,
+            num_expl_steps_per_train_loop,
+            num_trains_per_train_loop,
+            num_train_loops_per_epoch=1,
+            min_num_steps_before_training=0,
+    ):
+        self.trainer = trainer
+        self.expl_env = exploration_env
+        self.eval_env = evaluation_env
+        self.expl_data_collector = exploration_data_collector
+        self.eval_data_collector = evaluation_data_collector
+        self.replay_buffer = replay_buffer
+        self.batch_size = batch_size
+        self.max_path_length = max_path_length
+        self.num_epochs = num_epochs
+        self.num_eval_steps_per_epoch = num_eval_steps_per_epoch
+        self.num_trains_per_train_loop = num_trains_per_train_loop
+        self.num_train_loops_per_epoch = num_train_loops_per_epoch
+        self.num_expl_steps_per_train_loop = num_expl_steps_per_train_loop
+        self.min_num_steps_before_training = min_num_steps_before_training
+        self._start_epoch = 0
+        self.post_epoch_funcs = []
+
+        # Set up online data collector
+        self.expl_data_collector.set_online(True)
+
+    def train(self, start_epoch=0):
+        self._start_epoch = start_epoch
+        self._train()
+
+    def _train(self):
+        self.training_mode(False)
+        if self.min_num_steps_before_training > 0:
+            print('Exploration')
+            init_expl_paths = self.expl_data_collector.collect_new_paths(
+                self.max_path_length,
+                self.min_num_steps_before_training,
+                discard_incomplete_paths=False,
+            )
+            self.replay_buffer.add_paths(init_expl_paths)
+            self.expl_data_collector.end_epoch(-1)
+
+        for epoch in gt.timed_for(
+                range(self._start_epoch, self.num_epochs),
+                save_itrs=True,
+        ):
+            self.eval_data_collector.collect_new_paths(
+                self.max_path_length,
+                self.num_eval_steps_per_epoch,
+            )
+            gt.stamp('evaluation sampling')
+
+            self.expl_data_collector.start_collection()
+            for _ in range(self.num_trains_per_train_loop):
+                self.expl_data_collector.collect_new_step(
+                    self.max_path_length,
+                    self.num_expl_steps_per_train_loop,
+                )
+                gt.stamp('exploration sampling', unique=False)
+
+                self.training_mode(True)
+                for _ in range(self.num_trains_per_train_loop):
+                    train_data = self.replay_buffer.random_batch(self.batch_size)
+                    self.trainer.train(train_data)
+                gt.stamp('training', unique=False)
+                self.training_mode(False)
+
+            new_expl_paths = self.expl_data_collector.end_collection()
+            self.replay_buffer.add_paths(new_expl_paths)
+            gt.stamp('data storing', unique=False)
+
+            self._end_epoch(epoch)
+
+    def _end_epoch(self, epoch):
+        snapshot = self._get_snapshot()
+        logger.save_itr_params(epoch, snapshot)
+        gt.stamp('saving')
+        self._log_stats(epoch)
+
+        self.expl_data_collector.end_epoch(epoch)
+        self.eval_data_collector.end_epoch(epoch)
+        self.replay_buffer.end_epoch(epoch)
+        self.trainer.end_epoch(epoch)
+
+        for post_epoch_func in self.post_epoch_funcs:
+            post_epoch_func(self, epoch)
+
+    def _get_snapshot(self):
+        snapshot = {}
+        for k, v in self.trainer.get_snapshot().items():
+            snapshot['trainer/' + k] = v
+        for k, v in self.expl_data_collector.get_snapshot().items():
+            snapshot['exploration/' + k] = v
+        for k, v in self.eval_data_collector.get_snapshot().items():
+            snapshot['evaluation/' + k] = v
+        for k, v in self.replay_buffer.get_snapshot().items():
+            snapshot['replay_buffer/' + k] = v
+        return snapshot
+
+    def _log_stats(self, epoch):
+        logger.log("Epoch {} finished".format(epoch), with_timestamp=True)
+
+        """
+        Replay Buffer
+        """
+        logger.record_dict(
+            self.replay_buffer.get_diagnostics(),
+            prefix='replay_buffer/'
+        )
+
+        """
+        Trainer
+        """
+        logger.record_dict(self.trainer.get_diagnostics(), prefix='trainer/')
+
+        """
+        Exploration
+        """
+        logger.record_dict(
+            self.expl_data_collector.get_diagnostics(),
+            prefix='exploration/'
+        )
+        expl_paths = self.expl_data_collector.get_epoch_paths()
+        if hasattr(self.expl_env, 'get_diagnostics'):
+            logger.record_dict(
+                self.expl_env.get_diagnostics(expl_paths),
+                prefix='exploration/',
+            )
+        logger.record_dict(
+            eval_util.get_generic_path_information(expl_paths),
+            prefix="exploration/",
+        )
+        """
+        Evaluation
+        """
+        logger.record_dict(
+            self.eval_data_collector.get_diagnostics(),
+            prefix='evaluation/',
+        )
+        eval_paths = self.eval_data_collector.get_epoch_paths()
+        if hasattr(self.eval_env, 'get_diagnostics'):
+            logger.record_dict(
+                self.eval_env.get_diagnostics(eval_paths),
+                prefix='evaluation/',
+            )
+        logger.record_dict(
+            eval_util.get_generic_path_information(eval_paths),
+            prefix="evaluation/",
+        )
+
+        """
+        Misc
+        """
+        gt.stamp('logging')
+        logger.record_dict(_get_epoch_timings(epoch))
+        logger.record_tabular('Epoch', epoch)
+        logger.dump_tabular(with_prefix=False, with_timestamp=False)
+
+    def training_mode(self, mode):
+        """
+        Set training mode to `mode`.
+        :param mode: If True, training will happen (e.g. set the dropout
+        probabilities to not all ones).
+        """
+        raise NotImplementedError('training_mode must implemented by inherited class')
--- rlkit/samplers/data_collector.py
+++ rlkit/samplers/data_collector.py
@@ -1,19 +1,15 @@
 import abc
+import numpy as np
 from collections import deque, OrderedDict
 
 from rlkit.envs.vae_wrapper import VAEWrappedEnv
-from rlkit.core.eval_util import create_stats_ordered_dict
+from rlkit.data_management.path_builder import PathBuilder
 from rlkit.samplers.rollout_functions import rollout, multitask_rollout
 
 
 class PathCollector(object, metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def collect_new_paths(
-            self,
-            max_path_length,
-            num_steps,
-            discard_incomplete_paths,
-    ):
+    def collect_new_paths(self, max_path_length, num_steps):
         pass
 
     @abc.abstractmethod
@@ -36,6 +32,7 @@ class MdpPathCollector(PathCollector):
             env,
             policy,
             max_num_epoch_paths_saved=32,
+            online_mode=False,
     ):
         self._env = env
         self._policy = policy
@@ -44,6 +41,7 @@ class MdpPathCollector(PathCollector):
 
         self._num_steps_total = 0
         self._num_paths_total = 0
+        self.online_mode = online_mode
 
     def collect_new_paths(
             self,
@@ -102,6 +100,60 @@ class MdpPathCollector(PathCollector):
             policy=self._policy,
         )
 
+    def set_online(self, value=True):
+        self.online_mode = value
+
+    def start_collection(self):
+        assert self.online_mode, 'Step-wise collection is only enabled in online mode.'
+        self._current_path_builder = PathBuilder()
+        self._paths = []
+        self._obs = self._start_new_rollout()
+
+    def end_collection(self):
+        assert self.online_mode, 'Step-wise collection is only enabled in online mode.'
+        paths = self._paths
+        self._num_paths_total += len(paths)
+        self._num_steps_total += sum([ len(path['actions'] for path in paths]))
+        self._epoch_paths.extend(paths)
+        # clear paths
+        self._paths = []
+        return paths
+
+    def collect_new_step(self, max_path_length, num_steps):
+        assert self.online_mode, 'Step-wise collection is only enabled in online mode.'
+        # rollout step
+        action, agent_info = self._policy.get_action(self._obs)
+        next_ob, reward, terminal, env_info = (
+            self._env.step(action)
+        )
+        terminal = np.array([terminal])
+        reward = np.array([reward])
+        # store path obs
+        self._current_path_builder.add_all(
+            observations=self._obs,
+            actions=action,
+            rewards=reward,
+            next_observations=next_ob,
+            terminals=terminal,
+            agent_infos=agent_info,
+            env_infos=env_info,
+        )
+        if terminal or len(self._current_path_builder) >= max_path_length:
+            self._handle_rollout_ending()
+            self._obs = self._start_new_rollout()
+        else:
+            self._obs = next_ob
+
+    def _start_new_rollout(self):
+        return self._env.reset()
+
+    def _handle_rollout_ending(self):
+        if len(self._current_path_builder) > 0:
+            path = self._current_path_builder.get_all_stacked()
+            self._current_path_builder = PathBuilder()
+            self._paths.append(path)
+
+
 
 class GoalConditionedPathCollector(PathCollector):
     def __init__(
@@ -111,6 +163,7 @@ class GoalConditionedPathCollector(PathCollector):
             max_num_epoch_paths_saved=32,
             observation_key='observation',
             desired_goal_key='desired_goal',
+            online_mode=False,
     ):
         self._env = env
         self._policy = policy
@@ -121,6 +174,8 @@ class GoalConditionedPathCollector(PathCollector):
 
         self._num_steps_total = 0
         self._num_paths_total = 0
+        self.set_online(online_mode)
+        self._obs = None # cache variable
 
     def collect_new_paths(
             self,
@@ -128,17 +183,17 @@ class GoalConditionedPathCollector(PathCollector):
             num_steps,
             discard_incomplete_paths,
     ):
+        assert self.online_mode  == False
         paths = []
         num_steps_collected = 0
         while num_steps_collected < num_steps:
-            max_path_length_this_loop = min(  # Do not go over num_steps
-                max_path_length,
-                num_steps - num_steps_collected,
-                )
             path = multitask_rollout(
                 self._env,
                 self._policy,
-                max_path_length=max_path_length_this_loop,
+                max_path_length=min(  # Do not go over num_steps
+                    max_path_length,
+                    num_steps - num_steps_collected,
+                ),
                 observation_key=self._observation_key,
                 desired_goal_key=self._desired_goal_key,
                 return_dict_obs=True,
@@ -184,6 +239,63 @@ class GoalConditionedPathCollector(PathCollector):
             desired_goal_key=self._desired_goal_key,
         )
 
+    # Functions related to online mode
+    def set_online(self, value):
+        self.online_mode = value
+
+    def start_collection(self):
+        assert self.online_mode, 'Step-wise collection is only enabled in online mode.'
+        self._current_path_builder = PathBuilder()
+        self._paths = []
+        self._obs = self._start_new_rollout()
+
+    def end_collection(self):
+        assert self.online_mode, 'Step-wise collection is only enabled in online mode.'
+        paths = self._paths
+        self._num_paths_total += len(paths)
+        self._num_steps_total += sum([ len(path['actions'] for path in paths]))
+        self._epoch_paths.extend(paths)
+        # clear paths
+        self._paths = []
+        return paths
+
+    def collect_new_step(self, max_path_length, num_steps):
+        assert self.online_mode, 'Step-wise collection is only enabled in online mode.'
+        # rollout step
+        new_obs = np.hstack((
+            self._obs[self._observation_key],
+            self._obs[self._desired_goal_key],
+        ))
+        action, agent_info = self._policy.get_action(new_obs)
+        next_ob, reward, terminal, env_info = (
+            self._env.step(action)
+        )
+        terminal = np.array([terminal])
+        reward = np.array([reward])
+        # store path obs
+        self._current_path_builder.add_all(
+            observations=self._obs,
+            actions=action,
+            rewards=reward,
+            next_observations=next_ob,
+            terminals=terminal,
+            agent_infos=agent_info,
+            env_infos=env_info,
+        )
+        if terminal or len(self._current_path_builder) >= max_path_length:
+            self._handle_rollout_ending()
+            self._obs = self._start_new_rollout()
+        else:
+            self._obs = next_ob
+
+    def _start_new_rollout(self):
+        return self._env.reset()
+
+    def _handle_rollout_ending(self):
+        if len(self._current_path_builder) > 0:
+            path = self._current_path_builder.get_all_stacked()
+            self._current_path_builder = PathBuilder()
+            self._paths.append(path)
 
 class VAEWrappedEnvPathCollector(GoalConditionedPathCollector):
     def __init__(
@@ -198,7 +310,14 @@ class VAEWrappedEnvPathCollector(GoalConditionedPathCollector):
         self._goal_sampling_mode = goal_sampling_mode
         self._decode_goals = decode_goals
 
+<<<<<<< HEAD
     def collect_new_paths(self, *args, **kwargs):
         self._env.goal_sampling_mode = self._goal_sampling_mode
         self._env.decode_goals = self._decode_goals
         return super().collect_new_paths(*args, **kwargs)
+=======
+    def collect_new_paths(self, max_path_length, num_steps):
+        self._env.goal_sampling_mode = self._goal_sampling_mode
+        self._env.decode_goals = self._decode_goals
+        return super().collect_new_paths(max_path_length, num_steps)
+>>>>>>> Add online rl algorithm + train_mode function
