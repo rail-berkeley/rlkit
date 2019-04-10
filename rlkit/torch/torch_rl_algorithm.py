b6562e7997c1b8f0905b33e96ea98615aa1146a7
Hexiang Hu <frank.hexiang@gmail.com>
2019-04-09 22:29:22 -0700

    Add online rl algorithm + train_mode function
    Make compatible with most recent data_collector
    
    Fix Multiple Functions
    
    (1) Add train_mode to batch_rl_algorithm
    (2) Make online_rl_algorithm compatible with new data collector api

--- rlkit/torch/torch_rl_algorithm.py
+++ rlkit/torch/torch_rl_algorithm.py
@@ -2,9 +2,9 @@ import abc
 from typing import Iterable
 from torch import nn as nn
 
-from rlkit.core.rl_algorithm import BatchRLAlgorithm
+from rlkit.core.batch_rl_algorithm import BatchRLAlgorithm
+from rlkit.core.online_rl_algorithm import OnlineRLAlgorithm
 from rlkit.core.trainer import Trainer
-from rlkit.torch.core import np_to_pytorch_batch
 
 
 class TorchBatchRLAlgorithm(BatchRLAlgorithm, metaclass=abc.ABCMeta):
@@ -12,16 +12,21 @@ class TorchBatchRLAlgorithm(BatchRLAlgorithm, metaclass=abc.ABCMeta):
         for net in self.trainer.networks:
             net.to(device)
 
+    def training_mode(self, mode):
+        for net in self.trainer.networks:
+            net.train(mode)
+
+class TorchOnlineRLAlgorithm(OnlineRLAlgorithm, metaclass=abc.ABCMeta):
+    def to(self, device):
+        for net in self.trainer.networks:
+            net.to(device)
+
+    def training_mode(self, mode):
+        for net in self.trainer.networks:
+            net.train(mode)
+
 
 class TorchTrainer(Trainer, metaclass=abc.ABCMeta):
-    def train(self, np_batch):
-        batch = np_to_pytorch_batch(np_batch)
-        self.train_from_torch(batch)
-
-    @abc.abstractmethod
-    def train_from_torch(self, batch):
-        pass
-
     @property
     @abc.abstractmethod
     def networks(self) -> Iterable[nn.Module]:
