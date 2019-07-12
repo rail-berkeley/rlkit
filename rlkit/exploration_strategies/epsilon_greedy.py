60eb6a9eb5be3ccffb8187a8ae4548d9c805d736
Coline Devin <coline@berkeley.edu>
2019-07-12 10:44:24 -0700

    Added linear scheduling option to epsilon greedy exploration

--- rlkit/exploration_strategies/epsilon_greedy.py
+++ rlkit/exploration_strategies/epsilon_greedy.py
@@ -1,17 +1,31 @@
 import random
+import numpy as np
+
+from gym.spaces import Discrete
 
 from rlkit.exploration_strategies.base import RawExplorationStrategy
+from rlkit.util.ml_util import LinearSchedule
 
 
 class EpsilonGreedy(RawExplorationStrategy):
     """
     Take a random discrete action with some probability.
     """
-    def __init__(self, action_space, prob_random_action=0.1):
-        self.prob_random_action = prob_random_action
+    def __init__(self, action_space, action_prior=None, prob_init=0.1, prob_end=0.1, steps=1e6):
+        self.prob_random_action = LinearSchedule(prob_init, prob_end, steps)
         self.action_space = action_space
+        self.action_prior = action_prior
 
-    def get_action_from_raw_action(self, action, **kwargs):
-        if random.random() <= self.prob_random_action:
-            return self.action_space.sample()
+        assert (self.action_prior is None) or len(self.action_prior) == self.action_space.n
+
+    def get_exploration_action(self, action, **kwargs):
+        if self.action_prior is not None:
+            a = np.random.dirichlet(self.action_prior, size=1).argmax()
+        else:
+            a = self.action_space.sample()
+        return a
+
+    def get_action_from_raw_action(self, action,t=0):
+        if random.random() <= self.prob_random_action.get_value(t):
+            return self.get_exploration_action(action)
         return action
