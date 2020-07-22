ed2830923cdb6eefc838589ebac35ebc650335e8
Ian Char <ianchar4@gmail.com>
2020-07-22 11:35:29 -0400

    Corrected SAC get_snapshot. (#113)

--- rlkit/torch/sac/sac.py
+++ rlkit/torch/sac/sac.py
@@ -224,6 +224,6 @@ class SACTrainer(TorchTrainer):
             policy=self.policy,
             qf1=self.qf1,
             qf2=self.qf2,
-            target_qf1=self.qf1,
-            target_qf2=self.qf2,
+            target_qf1=self.target_qf1,
+            target_qf2=self.target_qf2,
         )
