dca12f5c38451ea81fea38e818edb7a43a9a2ab8
Murtaza Dalal <mdalal@compute-0-9.local>
2020-11-05 00:51:08 -0500

    sc

--- experiments/kitchen/run_exp.sh
+++ experiments/kitchen/run_exp.sh
@@ -2,5 +2,5 @@
 for value in {0..1} # number of GPUs
 do
   echo $value
-  CUDA_VISIBLE_DEVICES=$value python /home/mdalal/research/rlkit/experiments/kitchen/dreamer.py --exp_prefix test_sbatch --gpu_id $value --num_seeds 3 --debug --num_expl_envs 1 --tmux_session_name 0 --num_gpus 4 &
+  CUDA_VISIBLE_DEVICES=$value python /home/mdalal/research/rlkit/experiments/kitchen/dreamer.py --exp_prefix test_sbatch --gpu_id $value --num_seeds 3 --debug --num_expl_envs 1 --tmux_session_name 0 --num_gpus 4
 done
