fa1ca2a067f25abfb05895ba20b447b6c6e763b1
Murtaza Dalal <mdalal@andrew.cmu.edu>
2020-11-04 22:03:53 -0800

    run script update

--- experiments/kitchen/run_exp.sh
+++ experiments/kitchen/run_exp.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-for value in {0..1} # number of GPUs
+for value in {0..3} # number of GPUs
 do
-  echo $value
-  CUDA_VISIBLE_DEVICES=$value python /home/mdalal/research/rlkit/experiments/kitchen/dreamer.py --exp_prefix test_sbatch --gpu_id $value --num_seeds 3 --debug --num_expl_envs 1 --tmux_session_name 0 --num_gpus 4
+  CUDA_VISIBLE_DEVICES=$value python /home/mdalal/research/rlkit/experiments/kitchen/multitask_dreamer.py --exp_prefix kitchen_multitask_fixed_schema_sweep --gpu_id $value --num_seeds 3 --num_expl_envs 10 --num_gpus 4 &
+done
+
+for value in {0..7} # number of GPUs
+do
+  CUDA_VISIBLE_DEVICES=$value python /home/mdalal/research/rlkit/experiments/kitchen/dreamer.py --exp_prefix kitchen_fixed_schema_sweep --gpu_id $value --num_seeds 3 --num_expl_envs 10 --num_gpus 8 &
+done
+
+for value in {0..3} # number of GPUs
+do
+  CUDA_VISIBLE_DEVICES=$value python /home/mdalal/research/rlkit/experiments/kitchen/dreamer_learn_schema.py --exp_prefix kitchen_learn_schema_sweep --gpu_id $value --num_seeds 3 --num_expl_envs 10 --num_gpus 4 &
 done
