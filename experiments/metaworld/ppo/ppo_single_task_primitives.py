13b99d73316eb5ee3fe294dd9ff0c651365cdb43
Murtaza Dalal <mdalal@andrew.cmu.edu>
2021-05-09 00:41:40 -0400

    updated ppo mw exp

--- experiments/metaworld/ppo/ppo_single_task_primitives.py
+++ experiments/metaworld/ppo/ppo_single_task_primitives.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
             use_combined_action_space=True,
             action_scale=1,
             max_path_length=5,
-            reward_type="dense",
+            reward_type="sparse",
             usage_kwargs=dict(
                 use_dm_backend=True,
                 use_raw_action_wrappers=False,
@@ -61,7 +61,7 @@ if __name__ == "__main__":
         ),
         actor_kwargs=dict(recurrent=False, hidden_size=512, hidden_activation="relu"),
         num_processes=10,
-        num_env_steps=int(5e5),
+        num_env_steps=int(1e6),
         num_steps=2048 // 10,
         log_interval=1,
         eval_interval=1,
@@ -73,12 +73,21 @@ if __name__ == "__main__":
 
     search_space = {
         "env_name": [
-            "plate-slide-v2",
+            # solveable
             # 'basketball-v2',
-            # 'bin-picking-v2',
             # 'assembly-v2',
-            # 'soccer-v2',
+            # "disassemble-v2"
+            # #verified and medium
+            # "soccer-v2",
             # 'sweep-into-v2',
+            # easy
+            # "plate-slide-v2",
+            "drawer-close-v2",
+            # "faucet-open-v2",
+            # verified and unsolved:
+            # "bin-picking-v2",
+            # unverified and unsolved:
+            # "stick-pull-v2",
         ],
     }
     sweeper = hyp.DeterministicHyperparameterSweeper(
@@ -102,5 +111,5 @@ if __name__ == "__main__":
                 )[:-1],
                 seed=seed,
                 exp_id=exp_id,
-                skip_wait=True,
+                skip_wait=False,
             )
