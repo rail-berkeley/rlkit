987106e22179cfdeb06d04887994ce5e27023751
Murtaza Dalal <mdalal@andrew.cmu.edu>
2021-05-10 00:06:50 -0400

    added back original rlkit rlalgo

--- rlkit/torch/torch_rl_algorithm.py
+++ rlkit/torch/torch_rl_algorithm.py
@@ -4,7 +4,7 @@ from typing import Iterable
 
 from torch import nn as nn
 
-from rlkit.core.batch_rl_algorithm import BatchRLAlgorithm
+from rlkit.core.batch_rl_algorithm import BatchRLAlgorithm, BatchRLAlgorithmOrig
 from rlkit.core.online_rl_algorithm import OnlineRLAlgorithm
 from rlkit.core.trainer import Trainer
 from rlkit.torch.core import np_to_pytorch_batch
@@ -29,6 +29,14 @@ class TorchBatchRLAlgorithm(BatchRLAlgorithm):
         for net in self.trainer.networks:
             net.train(mode)
 
+class TorchBatchRLAlgorithmOrig(BatchRLAlgorithmOrig):
+    def to(self, device):
+        for net in self.trainer.networks:
+            net.to(device)
+
+    def training_mode(self, mode):
+        for net in self.trainer.networks:
+            net.train(mode)
 
 class TorchTrainer(Trainer, metaclass=abc.ABCMeta):
     def __init__(self):
